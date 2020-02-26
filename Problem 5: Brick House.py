4dc2c63571533fc5a27a906f6b4e900c1c757585
Harshita <57848574+HarshitaB2604@users.noreply.github.com>
2020-02-26 11:40:30 -0800

    Update and rename Problem 5.py to Problem 5: Brick House.py

---
+++ Problem 5: Brick House.py
@@ -0,0 +1,40 @@
+#*********************************FUNCTIONS*********************************
+def divide(mainArray):
+  shortBrick = []
+  longBrick = []
+  wallLenght = []
+
+  for i in range(len(mainArray)):
+    #find the two breaking points
+    longBrickApen = mainArray[i].find("n")
+    wallLenApen = mainArray[i].find("n", longBrickApen + 1, len(mainArray))
+    
+    shortBrick.append(mainArray[i][0])    
+    longBrick.append(mainArray[longBrickApen + 1])    
+    wallLenght.append(mainArray[wallLenApen + 1])
+
+  print(longBrick)
+
+
+def removeChar_Replace(a):
+  #removes the end line escape character at the end and replaces any spaces with n
+  for i in range(len(a)):
+    a[i] = a[i].replace("\n", "")
+    a[i] = a[i].replace(" ", "n")    
+
+
+#************************************MAIN**********************************
+#convert file to array
+inFile = open("Prob05.txt", "r")
+line = inFile.readline()
+
+prob05 = []
+while(line):
+  prob05.append(line)
+  line = inFile.readline()
+
+inFile.close()
+
+#processing the array
+removeChar_Replace(prob05)
+divide(prob05)
